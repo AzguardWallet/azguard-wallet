00b49df1a3c1a56fefa5f5fb6522d1a53a994561
257Byte <257byte@gmail.com>
2024-11-28 22:46:00 +0300

    Update contract, make use of `execute_with_setup`

--- src/wallet/services/account/contracts/azguard-v0.ts
+++ src/wallet/services/account/contracts/azguard-v0.ts
@@ -35,6 +35,7 @@ import { AzguardFunctionCall, IAccountContract } from '.';
 
 import compiled from './azguard-v0.json' with { type: "json" };
 const azguardV0Artifact = loadContractArtifact(compiled as NoirCompiledContract);
+const SETUP_CHUNK_SIZE = 2;
 const CHUNK_SIZE = 4;
 
 export class AzguardV0 implements IAccountContract {
@@ -72,11 +73,13 @@ export class AzguardV0 implements IAccountContract {
     }
 
     public buildTxExecutionRequest(pxe: PXE, calls: AzguardFunctionCall[], args: PackedValues[], nonce: Fr): Promise<TxExecutionRequest> {
-        return this._buildTxExecutionRequest(pxe, calls, args, nonce);
+        //return this._buildTxExecutionRequest(pxe, calls, args, nonce);
+        return this._buildTxExecutionRequestWithSetup(pxe, [], calls, args, nonce);
     }
 
     public buildTxSimulationRequest(pxe: PXE, call: AzguardFunctionCall, args: PackedValues): Promise<TxExecutionRequest> {
-        return this._buildTxExecutionRequest(pxe, [call], [args], Fr.zero());
+        //return this._buildTxExecutionRequest(pxe, [call], [args], Fr.zero());
+        return this._buildTxExecutionRequestWithSetup(pxe, [], [call], [args], Fr.zero());
     }
 
     private async _buildTxExecutionRequest(pxe: PXE, calls: AzguardFunctionCall[], args: PackedValues[], nonce: Fr): Promise<TxExecutionRequest> {
@@ -133,6 +136,99 @@ export class AzguardV0 implements IAccountContract {
         batchArgs.push(fnArgs);
 
         const payload = batchCalls.flatMap(x => x.toFields()).concat(nonce);
+        const payloadHash = poseidon2HashWithSeparator(payload, GeneratorIndex.SIGNATURE_PAYLOAD);
+        const signature = new Schnorr().constructSignature(payloadHash.toBuffer(), this.signingKey).toBuffer();
+        const authwit = new AuthWitness(payloadHash, [...signature]);
+        batchAuthwits.push(authwit);
+
+        const nodeInfo = await pxe.getNodeInfo();
+        const gasSettings = GasSettings.default();
+        const txContext = new TxContext(nodeInfo.l1ChainId, nodeInfo.protocolVersion, gasSettings);
+
+        const request = new TxExecutionRequest(this.address, fnSelector, fnArgs.hash, txContext, batchArgs, batchAuthwits);
+        
+        console.debug('registering account...');
+        await pxe.registerAccount(this.secret, computePartialAddress(this.instance));
+        console.debug('registering contract...');
+        await pxe.registerContract({instance: this.instance, artifact: azguardV0Artifact});
+
+        if (!await pxe.isContractInitialized(this.address)) {
+            console.debug('initialize account contract instance...');
+            return this._withInitialization(request);
+        }
+
+        return request;
+    }
+
+    private async _buildTxExecutionRequestWithSetup(pxe: PXE, setup: AzguardFunctionCall[], calls: AzguardFunctionCall[], args: PackedValues[], nonce: Fr): Promise<TxExecutionRequest> {
+        const fn = azguardV0Artifact.functions.find(x => x.name === "execute_with_setup")!
+        const fnSelector = FunctionSelector.fromNameAndParameters(fn.name, fn.parameters);
+        
+        let setupCalls = setup.slice();
+        let batchCalls = calls.slice();
+        const batchArgs = args.slice();
+        const batchAuthwits = [];
+
+        if (setupCalls.length > SETUP_CHUNK_SIZE) {
+            throw new Error("Unsuported number of setup calls");
+        }
+        while (batchCalls.length > CHUNK_SIZE) {
+            let emptySetup = new Array(SETUP_CHUNK_SIZE).fill(AzguardFunctionCall.empty());
+            let newCalls = [];
+            while (batchCalls.length >= CHUNK_SIZE) {
+                const chunkCalls = batchCalls.splice(0, CHUNK_SIZE);
+                const chunkNonce = nonce.isZero() ? Fr.zero() : Fr.random();
+                const chunkArgs = PackedValues.fromValues(encodeArguments(fn, [emptySetup, chunkCalls, chunkNonce]));
+                batchArgs.push(chunkArgs);
+                
+                const chunkPayload = emptySetup.flatMap(x => x.toFields())
+                    .concat(chunkCalls.flatMap(x => x.toFields()))
+                    .concat(chunkNonce);
+                const chunkPayloadHash = poseidon2HashWithSeparator(chunkPayload, GeneratorIndex.SIGNATURE_PAYLOAD);
+                const chunkSignature = new Schnorr().constructSignature(chunkPayloadHash.toBuffer(), this.signingKey).toBuffer();
+                const chunkAuthwit = new AuthWitness(chunkPayloadHash, [...chunkSignature]);
+                batchAuthwits.push(chunkAuthwit);
+                
+                newCalls.push(new AzguardFunctionCall(this.address, fnSelector, chunkArgs.hash, false, false));
+            }
+            if (newCalls.length % CHUNK_SIZE + batchCalls.length > CHUNK_SIZE) {
+                while (batchCalls.length < CHUNK_SIZE) {
+                    batchCalls.push(AzguardFunctionCall.empty());
+                }
+                const chunkCalls = batchCalls;
+                const chunkNonce = nonce.isZero() ? Fr.zero() : Fr.random();
+                const chunkArgs = PackedValues.fromValues(encodeArguments(fn, [emptySetup, chunkCalls, chunkNonce]));
+                batchArgs.push(chunkArgs);
+
+                const chunkPayload = emptySetup.flatMap(x => x.toFields())
+                    .concat(chunkCalls.flatMap(x => x.toFields()))
+                    .concat(chunkNonce);
+                const chunkPayloadHash = poseidon2HashWithSeparator(chunkPayload, GeneratorIndex.SIGNATURE_PAYLOAD);
+                const chunkSignature = new Schnorr().constructSignature(chunkPayloadHash.toBuffer(), this.signingKey).toBuffer();
+                const chunkAuthwit = new AuthWitness(chunkPayloadHash, [...chunkSignature]);
+                batchAuthwits.push(chunkAuthwit);
+
+                newCalls.push(new AzguardFunctionCall(this.address, fnSelector, chunkArgs.hash, false, false));
+            }
+            else {
+                newCalls.push(...batchCalls);
+            }
+            batchCalls = newCalls;
+        }
+
+        while (setupCalls.length < SETUP_CHUNK_SIZE) {
+            setupCalls.push(AzguardFunctionCall.empty());
+        }
+        while (batchCalls.length < CHUNK_SIZE) {
+            batchCalls.push(AzguardFunctionCall.empty());
+        }
+
+        const fnArgs = PackedValues.fromValues(encodeArguments(fn, [setupCalls, batchCalls, nonce]));
+        batchArgs.push(fnArgs);
+
+        const payload = setupCalls.flatMap(x => x.toFields())
+            .concat(batchCalls.flatMap(x => x.toFields()))
+            .concat(nonce);
         const payloadHash = poseidon2HashWithSeparator(payload, GeneratorIndex.SIGNATURE_PAYLOAD);
         const signature = new Schnorr().constructSignature(payloadHash.toBuffer(), this.signingKey).toBuffer();
         const authwit = new AuthWitness(payloadHash, [...signature]);
