c2e3e8f911ce3d422bd7ffe481bebe4ca1511bae
xdeq <celldwellerq@gmail.com>
2024-12-06 18:22:28 +0300

    fix (TokenMetadataPopup): update activeTokenIdx based on route.params.id

--- src/popup/components/popups/TokenMetadataPopup.vue
+++ src/popup/components/popups/TokenMetadataPopup.vue
@@ -21,6 +21,8 @@ const appStore = useAppStore()
 const popupStore = usePopupStore()
 const cacheStore = useCacheStore()
 
+const route = useRoute()
+
 const displaceIdx = computed(() => {
 	return popupStore.len - popupStore.popups.token_metadata
 })
@@ -30,33 +32,37 @@ const props = defineProps({
 	show: Boolean,
 })
 
-const token = computed(() =>
-	appStore.tokens.find((t) => t.id === cacheStore.activeTokenIdx)
-)
+const token = computed(() => {
+	// biome-ignore lint/suspicious/noDoubleEquals: <explanation>
+	return appStore.tokens.findLast(t => t.id == cacheStore.activeTokenIdx)
+})
 
 const handleCopyAddress = () => {
 	window.navigator.clipboard.writeText(token.value.contract)
 	openToast({ label: "Contract address is copied", icon: "copy" })
 }
+
+watch(
+	() => props.show,
+	() => {
+		if (props.show) {
+			if (route.params.id) {
+				cacheStore.activeTokenIdx = route.params.id
+			}
+		}
+	},
+)
 </script>
 
 <template>
-	<Popup
-		:show
-		@onClose="emit('onClose')"
-		:displaceIdx="popupStore.popups.token_metadata"
-	>
+	<Popup :show @onClose="emit('onClose')" :displaceIdx="popupStore.popups.token_metadata">
 		<PopupCard :displaceIdx>
 			<Flex wide direction="column" gap="20" :class="$style.wrapper">
-				<Text size="14" weight="600" color="primary">
-					Token Metadata
-				</Text>
+				<Text size="14" weight="600" color="primary"> Token Metadata </Text>
 
 				<Flex direction="column" gap="16">
 					<Flex align="center" justify="between">
-						<Text size="12" weight="600" color="tertiary">
-							Id
-						</Text>
+						<Text size="12" weight="600" color="tertiary"> Id </Text>
 
 						<Text size="12" weight="600" color="secondary">
 							{{ token.id }}
@@ -64,26 +70,16 @@ const handleCopyAddress = () => {
 					</Flex>
 
 					<Flex align="center" justify="between">
-						<Text size="12" weight="600" color="tertiary">
-							Contract Address
-						</Text>
+						<Text size="12" weight="600" color="tertiary"> Contract Address </Text>
 
-						<Text
-							@click="handleCopyAddress"
-							size="12"
-							weight="600"
-							color="secondary"
-							class="copyable"
-						>
+						<Text @click="handleCopyAddress" size="12" weight="600" color="secondary" class="copyable">
 							{{ token.contract.slice(0, 6) }} ...
 							{{ token.contract.slice(-4) }}
 						</Text>
 					</Flex>
 
 					<Flex align="center" justify="between">
-						<Text size="12" weight="600" color="tertiary">
-							Name
-						</Text>
+						<Text size="12" weight="600" color="tertiary"> Name </Text>
 
 						<Text size="12" weight="600" color="secondary">
 							{{ token.name }}
@@ -91,9 +87,7 @@ const handleCopyAddress = () => {
 					</Flex>
 
 					<Flex align="center" justify="between">
-						<Text size="12" weight="600" color="tertiary">
-							Symbol
-						</Text>
+						<Text size="12" weight="600" color="tertiary"> Symbol </Text>
 
 						<Text size="12" weight="600" color="secondary">
 							{{ token.symbol }}
@@ -101,78 +95,47 @@ const handleCopyAddress = () => {
 					</Flex>
 
 					<Flex align="center" justify="between">
-						<Text size="12" weight="600" color="tertiary">
-							Private Balances
-						</Text>
+						<Text size="12" weight="600" color="tertiary"> Private Balances </Text>
 
 						<Icon
-							:name="
-								token.hasPrivateBalances
-									? 'check-circle'
-									: 'close-circle'
-							"
+							:name="token.hasPrivateBalances ? 'check-circle' : 'close-circle'"
 							size="12"
 							:color="token.hasPrivateBalances ? 'green' : 'red'"
 						/>
 					</Flex>
 
 					<Flex align="center" justify="between">
-						<Text size="12" weight="600" color="tertiary">
-							Public Balances
-						</Text>
+						<Text size="12" weight="600" color="tertiary"> Public Balances </Text>
 
 						<Icon
-							:name="
-								token.hasPublicBalances
-									? 'check-circle'
-									: 'close-circle'
-							"
+							:name="token.hasPublicBalances ? 'check-circle' : 'close-circle'"
 							size="12"
 							:color="token.hasPublicBalances ? 'green' : 'red'"
 						/>
 					</Flex>
 
 					<Flex align="center" justify="between">
-						<Text size="12" weight="600" color="tertiary">
-							Public Transfers
-						</Text>
+						<Text size="12" weight="600" color="tertiary"> Public Transfers </Text>
 
 						<Icon
-							:name="
-								token.hasPublicTransfers
-									? 'check-circle'
-									: 'close-circle'
-							"
+							:name="token.hasPublicTransfers ? 'check-circle' : 'close-circle'"
 							size="12"
 							:color="token.hasPublicTransfers ? 'green' : 'red'"
 						/>
 					</Flex>
 
 					<Flex align="center" justify="between">
-						<Text size="12" weight="600" color="tertiary">
-							Private Transfers
-						</Text>
+						<Text size="12" weight="600" color="tertiary"> Private Transfers </Text>
 
 						<Icon
-							:name="
-								token.hasPrivateTransfers
-									? 'check-circle'
-									: 'close-circle'
-							"
+							:name="token.hasPrivateTransfers ? 'check-circle' : 'close-circle'"
 							size="12"
 							:color="token.hasPrivateTransfers ? 'green' : 'red'"
 						/>
 					</Flex>
 				</Flex>
 
-				<Button
-					@click="emit('onClose')"
-					wide
-					type="secondary"
-					size="medium"
-				>
-					Close
-				</Button>
+				<Button @click="emit('onClose')" wide type="secondary" size="medium"> Close </Button>
 			</Flex>
 		</PopupCard>
 	</Popup>
