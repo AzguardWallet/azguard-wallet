a1942ee069219dee8996544abf686dcaefeef685
xdeq <celldwellerq@gmail.com>
2024-12-09 18:02:09 +0300

    fix: redirect from options pages to popup pages

--- src/options/index.ts
+++ src/options/index.ts
@@ -6,10 +6,7 @@ import routes from "~pages"
 import "@/assets/styles/base.scss"
 import "./index.scss"
 
-routes.push({
-	path: "/",
-	redirect: "/options",
-})
+location.href = "/src/popup/index.html#/popup/general"
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -19,7 +16,5 @@ const router = createRouter({
 createApp(App).use(router).use(createPinia()).mount("#app")
 
 self.onerror = (message, source, lineno, colno, error) => {
-	console.info(
-		`Error: ${message}\nSource: ${source}\nLine: ${lineno}\nColumn: ${colno}\nError object: ${error}`
-	)
+	console.info(`Error: ${message}\nSource: ${source}\nLine: ${lineno}\nColumn: ${colno}\nError object: ${error}`)
 }
