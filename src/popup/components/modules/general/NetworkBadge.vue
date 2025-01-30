b74f50bb696179518be8ef898b0921c6b88413f4
xdeq <celldwellerq@gmail.com>
2025-01-30 23:08:08 +0300

    feat(NetworkBadge): tooltip with node id & url

--- src/popup/components/modules/general/NetworkBadge.vue
+++ src/popup/components/modules/general/NetworkBadge.vue
@@ -1,4 +1,8 @@
 <script setup>
+/** Store */
+import { useAppStore } from "@/stores/app.store"
+const appStore = useAppStore()
+
 /** Utils */
 import { getNetworkColor, getNetworkType } from "@/components/ui/utils.js"
 
@@ -8,6 +12,8 @@ const props = defineProps({
 		required: true,
 	},
 })
+
+const node = computed(() => appStore.networks.find(n => n.chainId === props.chainId))
 
 const chain = computed(() => {
 	const chainType = getNetworkType(props.chainId)
@@ -20,15 +26,29 @@ const chain = computed(() => {
 </script>
 
 <template>
-	<Flex align="center" gap="6" :class="$style.wrapper" :style="{ background: `var(--${getNetworkColor(chainId)})` }">
-		<Flex v-if="chain === 'Custom'" align="center" gap="2">
-			<Text size="11" weight="700"> {{ chain }} </Text>
-			<div :class="$style.divider" />
-			<Text size="10" weight="600"> {{ chainId }} </Text>
+	<Tooltip position="end">
+		<Flex
+			align="center"
+			gap="6"
+			:class="$style.wrapper"
+			:style="{ background: `var(--${getNetworkColor(chainId)})` }"
+		>
+			<Flex v-if="chain === 'Custom'" align="center" gap="2">
+				<Text size="11" weight="700"> {{ chain }} </Text>
+				<div :class="$style.divider" />
+				<Text size="10" weight="600"> {{ chainId }} </Text>
+			</Flex>
+
+			<Text v-else size="11" weight="700"> {{ chain }} </Text>
 		</Flex>
 
-		<Text v-else size="11" weight="700"> {{ chain }} </Text>
-	</Flex>
+		<template #content>
+			<Flex direction="column" gap="6" align="end">
+				<Text v-if="node"> <Text color="secondary">URL:</Text> {{ node.rpcUrl }} </Text>
+				<Text> <Text color="secondary">ID:</Text> {{ chainId }} </Text>
+			</Flex>
+		</template>
+	</Tooltip>
 </template>
 
 <style module>
