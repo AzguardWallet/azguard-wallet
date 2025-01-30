92f51cd18e6195471168cbc88ba556ebc0e9819a
xdeq <celldwellerq@gmail.com>
2025-01-30 23:46:11 +0300

    feat: new ui for account management

--- src/popup/pages/settings/general/accounts/index.vue
+++ src/popup/pages/settings/general/accounts/index.vue
@@ -11,6 +11,8 @@
 /** Components */
 import Navigation from "../../../../components/Navigation.vue"
 import Breadcrumbs from "@/components/ui/Settings/Breadcrumbs.vue"
+import ItemsContainer from "@/components/ui/Settings/ItemsContainer.vue"
+import SettingItem from "@/components/ui/Settings/SettingItem.vue"
 
 /** Composables */
 import { useToast } from "@/composables/toast"
@@ -38,6 +40,8 @@ const handleEditAccount = target => {
 }
 
 const handleHideAccount = acc => {
+	if (accounts.value.length === 1) return
+
 	appStore.changeAccountVisibility(acc, false)
 	openToast({ label: "Account successfully hidden" })
 }
@@ -63,57 +67,57 @@ const handleCopyAddress = target => {
 					Accounts &nbsp;<Text color="tertiary">{{ appStore.accounts.length }} </Text>
 				</Text>
 
-				<Flex direction="column" gap="6">
-					<Flex
-						v-for="acc in accounts"
-						@click="handleSelectAccount(acc)"
-						align="center"
-						justify="between"
-						:class="$style.card"
+				<ItemsContainer>
+					<SettingItem
+						v-for="account in accounts"
+						@click="handleSelectAccount(account)"
+						:title="account.name"
+						:description="`${account.address.slice(0, 6)}...${account.address.slice(-4)}`"
+						:icon="account?.address === appStore.account?.address ? 'check-circle' : 'vault'"
+						:iconFillColor="account?.address === appStore.account?.address ? 'blue' : 'tertiary'"
+						iconBgColor="transparent"
 					>
-						<Flex align="center" gap="10">
-							<Icon
-								:name="acc?.address === appStore.account?.address ? 'check-circle' : 'vault'"
-								size="16"
-								:color="acc?.address === appStore.account?.address ? 'green' : 'primary'"
-							/>
+						<template #right>
+							<Flex align="center" gap="8">
+								<Tooltip position="end" delay="350">
+									<Icon
+										@click.stop="handleCopyAddress(account.address)"
+										name="copy"
+										size="14"
+										color="tertiary"
+										:class="$style.icon_btn"
+									/>
 
-							<Text size="14" weight="600" color="primary" :class="$style.account_name">
-								{{ acc.name }}
-							</Text>
+									<template #content>Copy account address</template>
+								</Tooltip>
 
-							<Text
-								@click.stop="handleCopyAddress(acc.address)"
-								size="13"
-								weight="600"
-								color="tertiary"
-								class="copyable"
-								:class="$style.account_address"
-							>
-								{{ acc.address.slice(0, 6) }}
-								<Text color="dark">•••</Text>
-								{{ acc.address.slice(-4) }}
-							</Text>
-						</Flex>
+								<Tooltip position="end" delay="350">
+									<Icon
+										@click.stop="handleEditAccount(account)"
+										name="edit"
+										size="14"
+										color="tertiary"
+										:class="$style.icon_btn"
+									/>
 
-						<Flex align="center" gap="8" :class="$style.icons">
-							<Icon
-								@click.stop="handleEditAccount(acc)"
-								name="edit"
-								size="14"
-								color="tertiary"
-								:class="$style.icon_btn"
-							/>
-							<Icon
-								@click.stop="handleHideAccount(acc)"
-								name="close-circle"
-								size="14"
-								color="tertiary"
-								:class="$style.icon_btn"
-							/>
-						</Flex>
-					</Flex>
-				</Flex>
+									<template #content>Edit account</template>
+								</Tooltip>
+
+								<Tooltip position="end" delay="350">
+									<Icon
+										@click.stop="handleHideAccount(account)"
+										name="close-circle"
+										size="14"
+										color="tertiary"
+										:class="[$style.icon_btn, accounts.length === 1 && $style.disabled]"
+									/>
+
+									<template #content> Hide account </template>
+								</Tooltip>
+							</Flex>
+						</template>
+					</SettingItem>
+				</ItemsContainer>
 
 				<Button
 					@click="popupStore.open('new_account')"
@@ -127,55 +131,28 @@ const handleCopyAddress = target => {
 				</Button>
 			</Flex>
 
-			<Flex v-if="hiddenAccounts.length" direction="column" gap="16">
-				<Flex direction="column" gap="6">
-					<Flex align="center" justify="between">
-						<Text size="13" weight="600" color="body"> Hidden accounts </Text>
+			<Flex v-if="hiddenAccounts.length" direction="column" gap="12">
+				<Flex align="center" justify="between">
+					<Text size="13" weight="600" color="body"> Hidden accounts </Text>
 
-						<Text size="13" weight="600" color="secondary">
-							{{ hiddenAccounts.length }}
-						</Text>
-					</Flex>
+					<Text size="13" weight="600" color="secondary">
+						{{ hiddenAccounts.length }}
+					</Text>
+				</Flex>
 
-					<Flex
-						v-for="acc in hiddenAccounts"
-						@click="handleShowAccount(acc)"
-						align="center"
-						justify="between"
-						:class="$style.card"
+				<ItemsContainer description="Click on an account you want to make visible">
+					<SettingItem
+						v-for="account in hiddenAccounts"
+						@click="handleShowAccount(account)"
+						:title="account.name"
+						:description="account.address"
+						icon="vault"
 					>
-						<Flex align="center" gap="10">
-							<Icon
-								:name="acc?.address === appStore.account?.address ? 'check-circle' : 'vault'"
-								size="16"
-								:color="acc?.address === appStore.account?.address ? 'green' : 'tertiary'"
-							/>
-
-							<Text size="14" weight="600" color="secondary" :class="$style.account_name">
-								{{ acc.name }}
-							</Text>
-
-							<Text
-								@click.stop="handleCopyAddress(acc.address)"
-								size="13"
-								weight="600"
-								color="tertiary"
-								class="copyable"
-							>
-								{{ acc.address.slice(0, 6) }}
-								<Text color="dark">•••</Text>
-								{{ acc.address.slice(-4) }}
-							</Text>
-						</Flex>
-
-						<Icon name="arrow-back-up" size="14" color="secondary" />
-					</Flex>
-				</Flex>
-
-				<Flex align="center" gap="4">
-					<Icon name="info" size="12" color="support" />
-					<Text size="12" weight="600" color="support"> Click on the account you want to make visible </Text>
-				</Flex>
+						<template #right>
+							<Icon name="arrow-back-up" size="14" color="secondary" />
+						</template>
+					</SettingItem>
+				</ItemsContainer>
 			</Flex>
 		</Flex>
 
@@ -199,56 +176,16 @@ const handleCopyAddress = target => {
 	padding: 20px 24px 80px 24px;
 }
 
-.card {
-	border-radius: 12px;
-	cursor: pointer;
-	box-shadow: inset 0 0 0 1px var(--border), 0 1px 2px var(--shadow-5);
-
-	padding: 12px 16px 12px 12px;
-
-	transition: all 0.2s var(--bezier);
-
-	&:hover {
-		background: var(--gray-3);
-		box-shadow: inset 0 0 0 1px var(--border-hovered), 0 1px 2px var(--shadow-5);
-
-		& .icons {
-			opacity: 1;
-		}
-	}
-
-	&:active {
-		background: var(--gray-5);
-	}
-}
-
-.account_name {
-	max-width: 120px;
-
-	text-wrap: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
-}
-
-.account_address {
-	max-width: 110px;
-
-	text-wrap: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
-}
-
-.icons {
-	opacity: 0;
-
-	transition: all 0.2s var(--bezier);
-}
-
 .icon_btn {
 	transition: all 0.2s var(--bezier);
 
 	&:hover {
 		fill: var(--txt-primary);
+	}
+
+	&.disabled {
+		pointer-events: none;
+		opacity: 0.3;
 	}
 }
 </style>
